085646686f76fa69b6e6dc4afdcffce69fad8e7c
sampsonqian <szq5040@outlook.com>
2024-12-05 18:56:39 -0500

    A5

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -2,11 +2,12 @@ import Database from "../Database/index.js";
 export function enrollUserInCourse(userId, courseId) {
   const { enrollments } = Database;
   const newEnrollment = enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+  console.log(newEnrollment);
   return newEnrollment;
 }
 
-export function unenrollUserFromCourse(enrollmentId) {
+export function unenrollUserFromCourse(userId, courseId) {
     const { enrollments } = Database;
-    Database.enrollments = enrollments.filter(enrollment => !(enrollment._id === enrollmentId ));
+    Database.enrollments = enrollments.filter(enrollment => !(enrollment.course === courseId && enrollment.user == userId ));
   }
   
--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -1,30 +1,29 @@
 import * as enrollmentDao from "./dao.js";
-import * as courseDao from "../Courses/dao.js"
+import * as courseDao from "../Courses/dao.js";
 
 export default function EnrollmentRoutes(app) {
   // Enroll a user from a course
   app.post("/api/enrollments/:enrollmentId", async (req, res) => {
     const { userId, courseId } = req.body;
+    console.log(`Enroll Request: userId=${userId}, courseId=${courseId}`);
     const newEnrollment = await enrollmentDao.enrollUserInCourse(
       userId,
       courseId
     );
-    res.send(newEnrollment);
+    res.status(200).send({ message: "Enrolled successfully" });
+    //res.send(newEnrollment);
   });
 
   // Unenroll a user from a course
-  app.delete("/api/enrollments/:enrollmentId", async (req, res) => {
-    const { enrollmentId } = req.params;
-    const status = await enrollmentDao.unenrollUserFromCourse(enrollmentId);
+  app.delete("/api/enrollments/:userId/:courseId", async (req, res) => {
+    const { userId, courseId } = req.params;
+    const status = await enrollmentDao.unenrollUserFromCourse(userId, courseId );
     res.send(status);
   });
 
   app.get("/api/enrollments/:userId", async (req, res) => {
     const { userId } = req.params;
-
-
-
-    const enrollments = courseDao.findCoursesForEnrolledUser(userId);
+    const enrollments = await courseDao.findCoursesForEnrolledUser(userId);
     //  Database.enrollments.filter(
     //   (enrollment) => enrollment.user === userId
     // );
